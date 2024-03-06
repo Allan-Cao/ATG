290667a23af50db36b27093c6038337b0d4bdfc7
Allan Cao <a33cao@uwaterloo.ca>
2024-03-06 04:15:23 -0700

    Add pymysql to requirements.txt and create database module

---
+++ SQDBI/database/database.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the database connection details from environment variables
+DB_HOST = os.getenv("DATABASE_HOST")
+DB_PORT = os.getenv("DATABASE_PORT")
+DB_USERNAME = os.getenv("DATABASE_USERNAME")
+DB_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DB_NAME = os.getenv("DATABASE")
+SSL_CERT_PATH = os.getenv("SSL_CERT_PATH")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Create the SQLAlchemy engine with SSL configuration
+engine = create_engine(DATABASE_URL, echo=True, connect_args={"ssl": {"ca": SSL_CERT_PATH}})
+
+# Create a session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
