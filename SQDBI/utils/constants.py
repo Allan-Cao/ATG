95cfa6c3b07a990e9492496a8403e9e474fa48ae
Allan Cao <a33cao@uwaterloo.ca>
2024-03-29 02:51:05 -0600

    Add Side enum to constants.py

--- SQDBI/utils/constants.py
+++ SQDBI/utils/constants.py
@@ -1,6 +1,11 @@
 from cassiopeia import Patch
 from datetime import timedelta
+from enum import Enum
 
 DEFAULT_REGION = "NA"
 SEASON_START = Patch.from_str("14.1", region=DEFAULT_REGION).start
 MINIMUM_MATCH_DURATION = timedelta(minutes=10)
+
+class Side(Enum):
+    blue = 100
+    red = 200
