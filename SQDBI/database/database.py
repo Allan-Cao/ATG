d9fb6259fcb5b7e72c0a721aa077fd43bdd203cb
Allan Cao <a33cao@uwaterloo.ca>
2024-03-28 18:49:01 -0600

    Update database configuration and create all tables

--- SQDBI/database/database.py
+++ SQDBI/database/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
+from SQDBI.models.base import Base
 
 # Load environment variables from .env file
 load_dotenv()
@@ -12,15 +13,24 @@ DB_PORT = os.getenv("DATABASE_PORT")
 DB_USERNAME = os.getenv("DATABASE_USERNAME")
 DB_PASSWORD = os.getenv("DATABASE_PASSWORD")
 DB_NAME = os.getenv("DATABASE")
-SSL_CERT_PATH = os.getenv("SSL_CERT_PATH")
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# Unused after swapping to local database
+# SSL_CERT_PATH = os.getenv("SSL_CERT_PATH")
+# connect_args={"ssl": {"ca": SSL_CERT_PATH}}
 
-# Create the SQLAlchemy engine with SSL configuration
-engine = create_engine(DATABASE_URL, echo=True, connect_args={"ssl": {"ca": SSL_CERT_PATH}})
+DATABASE_URL = (
+    f"postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL, pool_recycle=3600)
+
+# Create all tables
+Base.metadata.create_all(bind=engine)
 
 # Create a session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 # Dependency
 def get_db():
