4be54aec95f798708bb5ab73d9cf62c5b4b1f174
Allan Cao <a33cao@uwaterloo.ca>
2024-03-29 02:51:18 -0600

    Add constants to utils module

--- SQDBI/utils/__init__.py
+++ SQDBI/utils/__init__.py
@@ -1,2 +1,3 @@
 from SQDBI.utils.update_validators import update_validator_patches
 from SQDBI.utils.update_accounts import update_player_accounts
+from SQDBI.utils.constants import Side, DEFAULT_REGION, SEASON_START, MINIMUM_MATCH_DURATION
