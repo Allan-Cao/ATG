1f5ead69adf07e5cc193b80a518d3b04fa4cf991
Allan Cao <a33cao@uwaterloo.ca>
2024-06-10 22:32:04 -0600

    Refactor utils/__init__.py to remove unused imports and constants

--- SQDBI/utils/__init__.py
+++ SQDBI/utils/__init__.py
@@ -1,3 +1,1 @@
-from SQDBI.utils.update_validators import update_validator_patches
-from SQDBI.utils.update_accounts import update_player_accounts
 from SQDBI.utils.constants import Side, DEFAULT_REGION, SEASON_START, MINIMUM_MATCH_DURATION
