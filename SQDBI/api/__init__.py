951d466ca1ef8738b9b8ea226dbb6f02eb0c9737
Allan Cao <allan@allancao.ca>
2024-07-14 01:47:03 -0600

    Adds GRID api route

--- SQDBI/api/__init__.py
+++ SQDBI/api/__init__.py
@@ -1,3 +1,4 @@
 from SQDBI.api.account_v1 import get_account_by_puuid, get_account_by_riot_id
 from SQDBI.api.match_v5 import get_available_matches, get_match_by_id, get_match_history
 from SQDBI.api.utils import get_match_string
+from SQDBI.api.grid_api import get_grid_riot_summary
---
+++ SQDBI/api/grid_api.py
@@ -0,0 +1,6 @@
+import requests as r
+
+def get_grid_riot_summary(series_id: str, game_number: int, GRID_API_KEY: str):
+    headers = {"x-api-key": GRID_API_KEY}
+    headers.update({'Accept': 'application/json'})
+    return r.get(f"https://api.grid.gg/file-download/end-state/riot/series/{series_id}/games/{game_number}/summary", headers=headers).json()
