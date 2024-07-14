d46f3a893025a4340e8e7749bb59c6f0be633b1c
Allan Cao <allan@allancao.ca>
2024-07-14 01:56:46 -0600

    Rewrites update_validator_patches to use gspread

--- SQDBI/utils/update_validators.py
+++ SQDBI/utils/update_validators.py
@@ -1,5 +1,5 @@
 from cassiopeia import Patch
-from pygsheets import Worksheet
+from gspread import Worksheet
 from typing import Optional
 
 
@@ -9,4 +9,4 @@ def update_validator_patches(
     if latest_patch is None:
         latest_patch = Patch.latest(region)
     patch = [f"{latest_patch.major}.{int(latest_patch.minor) - i}" for i in range(3)]
-    sh.worksheet("title", "validators").update_values("C2", list(map(list, zip(patch))))
+    sh.worksheet('validators').update('C2', list(map(list, zip(patch))))
