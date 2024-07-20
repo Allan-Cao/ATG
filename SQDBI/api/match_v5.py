cf27355f74ffacf3340b93862dd032544d0b9dbf
Allan Cao <allan@allancao.ca>
2024-07-20 00:53:04 -0600

    Adds typing to match_v5 wrapper

--- SQDBI/api/match_v5.py
+++ SQDBI/api/match_v5.py
@@ -1,10 +1,12 @@
 import requests as r
+from requests import Response
 from .utils import headers, routing
 from ratelimit import limits, RateLimitException
 from backoff import on_predicate, runtime
 
 MAX_CALLS_PER_TEN_SECONDS = 2000
 TEN_SECONDS = 10
+
 
 @on_predicate(
     runtime,
@@ -13,7 +15,9 @@ TEN_SECONDS = 10
     jitter=None,
 )
 @limits(calls=MAX_CALLS_PER_TEN_SECONDS, period=TEN_SECONDS)
-def get_match_by_id(match_id: str, region: str, api_key: str, timeline: bool = False) -> dict:
+def get_match_by_id(
+    match_id: str, region: str, api_key: str, timeline: bool = False
+) -> Response:
     if region not in routing.keys():
         raise ValueError(
             "Invalid region. Expecting NA/EUW/EUNE/KR/BR/LAN/LAS/TR/RU/OCE/JP/SEA"
@@ -26,6 +30,7 @@ def get_match_by_id(match_id: str, region: str, api_key: str, timeline: bool = F
     # response.raise_for_status()
     return response
 
+
 @on_predicate(
     runtime,
     predicate=lambda r: r.status_code == 429,
@@ -33,7 +38,7 @@ def get_match_by_id(match_id: str, region: str, api_key: str, timeline: bool = F
     jitter=None,
 )
 @limits(calls=MAX_CALLS_PER_TEN_SECONDS, period=TEN_SECONDS)
-def get_available_matches(puuid: str, region: str, api_key: str, **kwargs) -> list:
+def get_available_matches(puuid: str, region: str, api_key: str, **kwargs) -> Response:
     response = r.get(
         f"https://{routing[region]}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?api_key={api_key}",
         headers=headers,
@@ -42,17 +47,23 @@ def get_available_matches(puuid: str, region: str, api_key: str, **kwargs) -> li
     # response.raise_for_status()
     return response
 
+
 def get_match_history(puuid: str, region: str, api_key: str, **kwargs) -> list:
     start = 0
     count = 100
     matches = []
     while True:
         match_list = get_available_matches(
-            puuid=puuid, region=region, api_key=api_key, start=start, count=count, **kwargs
+            puuid=puuid,
+            region=region,
+            api_key=api_key,
+            start=start,
+            count=count,
+            **kwargs,
         ).json()
         if len(match_list) == 0:
             break
         matches.extend(match_list)
         start += count
-    
+
     return matches
