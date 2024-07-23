4644c548ef29c21c988a38121c70b1a766e378ee
Allan Cao <allan@allancao.ca>
2024-07-23 06:40:10 -0600

    Corrects envirnoment variable naming scheme

--- main.py
+++ main.py
@@ -3,26 +3,30 @@ from dotenv import dotenv_values
 
 # Load environment variables
 config = {
-    **dotenv_values(".env.shared"),
+    **dotenv_values(".shared.env"),
     **os.environ,
 }
 
-config.update(dotenv_values(".env.admin"))
+config.update(dotenv_values(".admin.env"))
 
 os.environ.update(config)
 
 import cassiopeia as cass
 from cassiopeia import Patch
-from SQDBI.match_lib.ingest_match import upsert_match_history
+from SQDBI.match_lib import upsert_match_history
 from SQDBI.database import Session
 from SQDBI.models import Player
 
 
 RIOT_API = os.environ.get("RIOT_API")
+
+if RIOT_API is None:
+    print("Riot API is not set.")
+    exit()
 cass.set_riot_api_key(RIOT_API)
 
 with Session() as session:
     PLAYERS = session.query(Player).all()
     # For now, we do insertion synchronously
     for player in PLAYERS:
-        upsert_match_history(player, RIOT_API)
+        upsert_match_history(session, player, RIOT_API)
