0e37ec0d07c8e3cd780732b7b2471fa41ee87a0e
Allan Cao <allan@allancao.ca>
2024-07-23 03:27:49 -0600

    Adds Lane enum

--- SQDBI/utils/constants.py
+++ SQDBI/utils/constants.py
@@ -6,6 +6,15 @@ DEFAULT_REGION = "NA"
 SEASON_START = Patch.from_str("14.1", region=DEFAULT_REGION).start
 MINIMUM_MATCH_DURATION = timedelta(minutes=10)
 
+
 class Side(Enum):
     blue = 100
     red = 200
+
+
+class Lane(Enum):
+    TOP = "top_lane"
+    JNG = "jungle"
+    MID = "mid_lane"
+    BOT = "bot_lane"
+    SUP = "utility"
