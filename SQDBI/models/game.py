24c66279f6aa1b47fadef0969cb4c1856024b328
Allan Cao <allan@allancao.ca>
2024-07-24 00:13:00 -0600

    Adds note on game_type changes

--- SQDBI/models/game.py
+++ SQDBI/models/game.py
@@ -17,8 +17,9 @@ class Game(Base):
     game_start: Mapped[int] = mapped_column(BigInteger)
     game_end: Mapped[int] = mapped_column(BigInteger)
     game_duration: Mapped[int] = mapped_column(Integer)  # in seconds
-
-    game_type: Mapped[str] = mapped_column(String(50))
+    game_type: Mapped[str] = mapped_column(
+        String(50)
+    )  # This should be "SOLOQUEUE" for Queueid 420 games and SCRIM/ESPORTS for TR games
     game_version_major: Mapped[int] = mapped_column(Integer())
     game_version_minor: Mapped[int] = mapped_column(Integer())
 
