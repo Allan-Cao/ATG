50a7218beb02b53c56c9839eb40c57bcdb47f9c8
Allan Cao <allan@allancao.ca>
2024-07-27 23:07:21 -0600

    Updates players typing and adds disambiguation column

--- SQDBI/models/player.py
+++ SQDBI/models/player.py
@@ -8,7 +8,11 @@ class Player(Base):
     __tablename__ = "players"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    # Not sure how to deal with naming conflicts. For now, we assume that names can be repeated but we can use the column below.
     name: Mapped[str] = mapped_column(String(255))
+    # We store the name used on Leaguepedia/Oracles Elixer here for easier use with those services.
+    # For now, this should be their Leaguepedia disambiguation name
+    disambiguation: Mapped[Optional[str]] = mapped_column(String(255), unique=True)
 
     accounts: Mapped[Optional[List["Account"]]] = relationship(
         "Account", back_populates="player"
@@ -21,4 +25,6 @@ class Player(Base):
     )
 
     def __repr__(self) -> str:
-        return f"<Player(id='{self.id}', name='{self.name}')>"
+        if self.disambiguation is None:
+            return f"{self.name}:{self.id}"
+        return f"{self.name}-{self.disambiguation}:{self.id}"
