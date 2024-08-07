aa23f9e21eaf4ae97222af25b7f0211911234a85
Allan Cao <allan@allancao.ca>
2024-08-07 09:56:00 -0600

    renames function in init file too oops

--- SQDBI/match_lib/__init__.py
+++ SQDBI/match_lib/__init__.py
@@ -1,6 +1,6 @@
 from .ingest_match import upsert_match_history, update_player_accounts, upsert_match
 from .match_helper import (
-    extract_major_minor_version,
+    extract_major_minor_patch,
     parse_participant_dictionary,
     process_match_metadata,
 )
