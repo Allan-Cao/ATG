60ae4fdc3df00e88710b378fe5807b8f589fd923
Allan Cao <allan@allancao.ca>
2024-10-04 17:02:56 -0600

    Correctly implements the retry rate limit using @on_predicate & removes API exposing f-string

--- SQDBI/api/account_v1.py
+++ SQDBI/api/account_v1.py
@@ -1,35 +1,41 @@
 import requests as r
+from requests import Response
 from .utils import headers
-from ratelimit import limits, RateLimitException, sleep_and_retry
+from ratelimit import limits, RateLimitException
+from backoff import on_predicate, runtime
 
 MAX_CALLS_PER_MINUTE = 1000
 ONE_MINUTE = 60
 
 
-@sleep_and_retry
+@on_predicate(
+    runtime,
+    predicate=lambda r: r.status_code == 429,
+    value=lambda r: int(r.headers.get("Retry-After")),
+    jitter=None,
+)
 @limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
-def get_account_by_riot_id(game_name: str, tag_line: str, api_key: str) -> dict:
+def get_account_by_riot_id(game_name: str, tag_line: str, api_key: str) -> Response:
     _headers = {"X-Riot-Token": api_key, **headers}
     # There are three routing values for account-v1; americas, asia, and europe. You can query for any account in any region. We recommend using the nearest cluster.
     response = r.get(
         f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}",
         headers=_headers,
     )
-
-    response.raise_for_status()
-
-    return response.json()
+    return response
 
 
-@sleep_and_retry
+@on_predicate(
+    runtime,
+    predicate=lambda r: r.status_code == 429,
+    value=lambda r: int(r.headers.get("Retry-After")),
+    jitter=None,
+)
 @limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
-def get_account_by_puuid(puuid: str, api_key: str) -> dict:
+def get_account_by_puuid(puuid: str, api_key: str) -> Response:
     _headers = {"X-Riot-Token": api_key, **headers}
     response = r.get(
         f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-puuid/{puuid}",
         headers=_headers,
     )
-
-    response.raise_for_status()
-
-    return response.json()
+    return response
