0c405c026f1db5fb41b7178dc982a17b8f3ff814
Allan Cao <allan@allancao.ca>
2024-10-04 17:08:14 -0600

    Adds more-detailed comments to the player.py model

--- SQDBI/models/player.py
+++ SQDBI/models/player.py
@@ -9,10 +9,12 @@ class Player(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     # Not sure how to deal with naming conflicts. For now, we assume that names can be repeated but we can use the column below.
     name: Mapped[str] = mapped_column(String(255))
+    # These *should* be unique and defined but we are storing non-GRID tracked players hence it should be nullable.
+    grid_id: Mapped[str | None] = mapped_column(String(50), unique=True, nullable=True)
     # We store the name used on Leaguepedia/Oracles Elixer here for easier use with those services.
     # For now, this should be their Leaguepedia disambiguation name
     disambiguation: Mapped[str | None] = mapped_column(String(255), unique=True)
-
+    # We store the associated RIOT Esports API accounts in the account table too in a *weird* but appropriate format
     accounts: Mapped[list["Account"]] = relationship("Account", back_populates="player")
 
     def __repr__(self) -> str:
