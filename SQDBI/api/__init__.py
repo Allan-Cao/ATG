d0883fb80a4bf7a681837a3eebcddd7f81f2d7aa
Allan Cao <allan@allancao.ca>
2024-11-05 17:25:10 -0700

    Corrects relative imports for api submodule

--- SQDBI/api/__init__.py
+++ SQDBI/api/__init__.py
@@ -1,5 +1,16 @@
-from SQDBI.api.account_v1 import get_account_by_puuid, get_account_by_riot_id
-from SQDBI.api.match_v5 import get_available_matches, get_match_by_id, get_match_history
-from SQDBI.api.utils import get_match_string
-from SQDBI.api.grid_api import get_grid_riot_summary
-from SQDBI.api.spectator_v5 import get_active_games
+from .account_v1 import get_account_by_puuid, get_account_by_riot_id
+from .match_v5 import get_available_matches, get_match_by_id, get_match_history
+from .utils import get_match_string
+from .grid_api import get_grid_riot_summary
+from .spectator_v5 import get_active_games
+
+__all__ = [
+    "get_account_by_puuid",
+    "get_account_by_riot_id",
+    "get_available_matches",
+    "get_match_by_id",
+    "get_match_history",
+    "get_match_string",
+    "get_grid_riot_summary",
+    "get_active_games",
+]
