feeb8bc5b63163f00a8e73faf6f2a8cdc6f111b6
Allan Cao <allan@allancao.ca>
2024-11-05 18:03:03 -0700

    Bump version number to reflect breaking change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SQDBI",
-    version="0.0.4.1",
+    version="0.1.0.1",
     packages=find_packages(include=["SQDBI", "SQDBI.*"]),
     install_requires=[
         "SQLAlchemy",
