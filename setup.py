84d94f7ba503b046b89701e8259c6c5e1914d806
Allan Cao <allan@allancao.ca>
2024-11-05 17:04:47 -0700

    Bump version number to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SQDBI",
-    version="0.0.2",
+    version="0.0.3",
     packages=["SQDBI"],
     install_requires=[
         "SQLAlchemy",
