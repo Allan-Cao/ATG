cb07066cd81659153f28b1233e45b84da1515d89
Allan Cao <allan@allancao.ca>
2024-11-05 17:05:31 -0700

    testing removing all imports from base __init__.py

--- SQDBI/__init__.py
+++ SQDBI/__init__.py
@@ -1,5 +1,5 @@
-from . import api
-from . import database
-from . import match_lib
-from . import models
-from . import utils
+# from . import api
+# from . import database
+# from . import match_lib
+# from . import models
+# from . import utils
