6b9b32ea90b098f7a16de3c73fefac9263ce97da
Allan Cao <allan@allancao.ca>
2025-02-01 20:48:30 -0700

    Adds ability to use read_only connection and removes inappropriate psycopg2 dependency

--- ATG/database/database.py
+++ ATG/database/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from ..models import Base
 
 
-def get_session_factory(database_url: str):
+def get_session_factory(database_url: str, read_only: bool = False):
     engine = create_engine(database_url, pool_recycle=3600)
-    Base.metadata.create_all(bind=engine)
+    if not read_only:
+        Base.metadata.create_all(bind=engine)
     return sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="ATG",
-    version="0.1.0.1",
+    version="0.1.0.2",
     packages=find_packages(include=["ATG", "ATG.*"]),
     install_requires=[
         "SQLAlchemy",
         "tqdm",
         "backoff",
-        "psycopg2-binary",
         "ratelimit",
         "requests",
     ],
