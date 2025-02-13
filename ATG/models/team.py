a67acc623ce36e8ab74d71fbdfcaf1b54bdaa7e7
Allan Cao <allan@allancao.ca>
2025-02-12 21:50:13 -0700

    BREAKING CHANGE: renames additional_information to additional_details and allows team_code to be nullable

--- ATG/models/team.py
+++ ATG/models/team.py
@@ -11,12 +11,12 @@ class Team(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(Text, unique=True)
     # Multiple teams can share the same team code due to inconsistancies in GRID/Bayes data e.x. academy teams share team codes with their main team
-    team_code: Mapped[str] = mapped_column(Text, unique=False)
+    team_code: Mapped[str | None] = mapped_column(Text, unique=False, nullable=True)
     # We can store GRID, LOL IDs here
     external_ids = mapped_column(JSONB)
 
     # We store additional information in a JSONB blob
-    additional_information = mapped_column(JSONB)
+    additional_details = mapped_column(JSONB)
 
     player_associations: Mapped[list["PlayerTeamAssociation"]] = relationship(
         "PlayerTeamAssociation", back_populates="team"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ATG",
-    version="1.0.1.0",
+    version="1.0.2.0",
     packages=find_packages(include=["ATG", "ATG.*"]),
     install_requires=[
         "SQLAlchemy",
