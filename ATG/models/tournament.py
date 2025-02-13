c84ee0b75d1392f2abdcfaae2a7a4a8057b61905
Allan Cao <allan@allancao.ca>
2025-02-12 23:25:43 -0700

    BREAKING CHANGE: Refactor tournament model to be more flexible

--- ATG/models/tournament.py
+++ ATG/models/tournament.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from sqlalchemy import Text, Integer, DateTime
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.types import SmallInteger
 from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
@@ -7,13 +8,11 @@ from .base import Base
 
 class Tournament(Base):
     __tablename__ = "tournaments"
+    # Same as with team, for consistancy, we use an integer id primary key.
+    # Practically, we store GRID ID's here (even though GRID IDs are typed as string by GRID)
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    # GRID Tournament ID
-    grid_id: Mapped[str | None] = mapped_column(Text, nullable=True)
     # Tournament Name
     name: Mapped[str] = mapped_column(Text)
-    # GRID offers a "shortened name" limited to 30 characters
-    shortened_name: Mapped[str | None] = mapped_column(Text, nullable=True)
     # We should automatically parse the tournament year / league / split from its name for easy searching
     year: Mapped[int | None] = mapped_column(SmallInteger, nullable=True)
     league: Mapped[str | None] = mapped_column(Text, nullable=True)
@@ -22,3 +21,6 @@ class Tournament(Base):
     # For now, these are generally NULL, in the future we should automatically set based on first/last games
     start_date: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
     end_date: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
+
+    # Additional tournament information can be stored here
+    tournament_details = mapped_column(JSONB)
