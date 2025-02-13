87e3544f20108db0120d471b475c694296591d4b
Allan Cao <allan@allancao.ca>
2025-02-13 13:26:40 -0700

    fix: enables autoflush

--- ATG/database/database.py
+++ ATG/database/database.py
@@ -7,4 +7,4 @@ def get_session_factory(database_url: str, read_only: bool = False):
     engine = create_engine(database_url, pool_recycle=3600)
     if not read_only:
         Base.metadata.create_all(bind=engine)
-    return sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    return sessionmaker(autocommit=False, autoflush=True, bind=engine)
