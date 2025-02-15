55da21e067b942db593a5107a15fb888cdf022f4
Allan Cao <allan@allancao.ca>
2025-02-14 22:26:08 -0700

    chore: bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ATG",
-    version="1.0.5.4",
+    version="1.0.5.5",
     packages=find_packages(include=["ATG", "ATG.*"]),
     install_requires=[
         "SQLAlchemy",
