789e08c0d1eac1040d97d185f9a73162c4991fe8
Allan Cao <allan@allancao.ca>
2025-03-30 21:41:52 -0600

    fix: Remove unused game_participants reference from import

--- ATG/models/__init__.py
+++ ATG/models/__init__.py
@@ -4,7 +4,6 @@ from .champion import Champion
 from .draft_event import DraftEvent
 from .game import Game
 from .game_event import GameEvent
-from .game_participant import GameParticipant
 from .player import Player
 from .participant import Participant
 from .player_team_association import PlayerTeamAssociation
@@ -19,7 +18,6 @@ __all__ = [
     "DraftEvent",
     "Game",
     "GameEvent",
-    "GameParticipant",
     "Player",
     "Participant",
     "PlayerTeamAssociation",
