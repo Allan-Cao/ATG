380d580b5ec72a026520c129516a55f9e76fc32c
Allan Cao <allan@allancao.ca>
2025-05-06 18:19:45 -0600

    fix: Remove match_helper imports

--- ATG/match_lib/__init__.py
+++ ATG/match_lib/__init__.py
@@ -3,17 +3,9 @@ from .ingest_match import (
     update_account_names,
     upsert_match,
 )
-from .match_helper import (
-    extract_major_minor_patch,
-    parse_participant_dictionary,
-    process_match_metadata,
-)
 
 __all__ = [
     "upsert_match_history",
     "update_account_names",
     "upsert_match",
-    "extract_major_minor_patch",
-    "parse_participant_dictionary",
-    "process_match_metadata",
 ]
