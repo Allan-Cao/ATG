0c0ec3974a978c0309d4310b564df46083d3bc41
Allan Cao <allan@allancao.ca>
2025-05-31 22:42:22 -0600

    chore: bump minor version number to reflect changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ATG",
-    version="1.3.4.0",
+    version="1.4.0.0",
     packages=find_packages(include=["ATG", "ATG.*"]),
     install_requires=[
         "SQLAlchemy",
