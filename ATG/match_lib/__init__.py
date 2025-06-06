294f86c8a434b7759aff1f32991554e078f77926
Allan Cao <allan@allancao.ca>
2025-06-05 21:08:11 -0600

    feat: Helper functions for various esports data processing tasks

--- ATG/match_lib/__init__.py
+++ ATG/match_lib/__init__.py
@@ -1,3 +1,10 @@
+from .esports_ingest import (
+    create_account,
+    create_player,
+    create_missing_accounts,
+    parse_game_participants,
+    get_common_prefix_length
+)
 from .ingest_match import (
     insert_match_history,
     update_account_names,
@@ -5,6 +12,11 @@ from .ingest_match import (
 )
 
 __all__ = [
+    "create_account",
+    "create_player",
+    "create_missing_accounts",
+    "parse_game_participants",
+    "get_common_prefix_length",
     "insert_match_history",
     "update_account_names",
     "process_match",
