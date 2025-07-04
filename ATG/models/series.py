c23f9c601d9d7346718bee404cc71403c9726f0c
Allan Cao <allan@allancao.ca>
2025-07-04 13:16:06 -0600

    feat: Change scheduled_start_time to be not nullable on series table

--- ATG/models/series.py
+++ ATG/models/series.py
@@ -5,14 +5,14 @@ from datetime import datetime
 from .base import Base
 from ..utils.constants import SeriesStatus, SeriesType
 
+
 class Series(Base):
     __tablename__ = "series"
     ###          1:1 GRID          ###
     id: Mapped[str] = mapped_column(Text, primary_key=True)
-    type: Mapped[SeriesType] # GRID SeriesType
-    # Some of the older series have null start times when data is not available
-    scheduled_start_time: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
-    format: Mapped[int] = mapped_column(Integer) # Best of number
+    type: Mapped[SeriesType]  # GRID SeriesType
+    scheduled_start_time: Mapped[datetime] = mapped_column(DateTime)
+    format: Mapped[int] = mapped_column(Integer)  # Best of number
     tournament_id: Mapped[str] = mapped_column(Text, ForeignKey("tournaments.id"))
     external_links = mapped_column(JSONB)
 
@@ -20,4 +20,6 @@ class Series(Base):
     status: Mapped[SeriesStatus]
 
     ###           Debug            ###
-    updated: Mapped[datetime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
+    updated: Mapped[datetime] = mapped_column(
+        DateTime, default=func.now(), onupdate=func.now()
+    )
